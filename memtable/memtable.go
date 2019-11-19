5a1b696d49b4176b9529662104ba95a474702cb7
dfdeshom <dfdeshom@gmail.com>
2019-11-19 17:55:17 -0600

    pkg stuff

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -1,38 +1,33 @@
 package main
 
-//package memtable
-
 import (
 	"fmt"
 
-	kv "github.com/dfdeshom/toykv/keyvaluestore"
+	//kv "github.com/dfdeshom/toykv"
+	"github.com/dfdeshom/toykv"
 	"github.com/google/btree"
 )
+
+type Entry toykv.Entry
 
 type MemTable struct {
 	btree btree.BTree
 }
 
-func (m MemTable) Get(key string) (kv.Entry, error) {
-	item := m.btree.Get(key.(Item))
+func (m MemTable) Get(key string) (string, error) {
+	k := Entry{Key: key}
+	item := m.btree.Get(k)
 	if item != nil {
 		return item.(Entry).Value, nil
 	}
-	return nil, nil
+	return "", nil
 }
-
-// type Entry struct {
-// 	Key    string
-// 	Value  string
-// 	IsLive bool
-// 	Offset int
-// }
 
 func (a Entry) Less(b btree.Item) bool {
 	return a.Key < b.(Entry).Key
 }
 
-type EntryIterator func(i Entry) bool
+type EntryIterator func(i toykv.Entry) bool
 
 func main() {
 	btreeDegree := 32
@@ -48,10 +43,8 @@ func main() {
 
 	tr.ReplaceOrInsert(e)
 	tr.ReplaceOrInsert(f)
-	fmt.Printf("%#v", tr)
+	fmt.Printf("%#v", tr.Get(e))
 
-	//it := btree.ItemIterator
-	//out := make([]btree.Item, 1)
 	tr.Ascend(func(i btree.Item) bool {
 		fmt.Printf("\n%#v\n", i.(Entry))
 		return true
